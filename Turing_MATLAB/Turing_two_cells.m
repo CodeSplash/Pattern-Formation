493d21c830d7d5fb046525971348e5dbf592e1b0
hugobowne <hugobowne@gmail.com>
2015-11-04 11:23:37 -0500

    added MATLAB code for pattern formation in the two-cell turing system (1952 formulation)

---
+++ Turing_MATLAB/Turing_two_cells.m
@@ -0,0 +1,25 @@
+% Here we SOLVE the system of DEs that 
+% describes Turing pattern formation in (Turing, 1952)
+% for the 2 cell, discrete system.
+% We use ode15s but, for most parameters, ode45 should work.
+%[T,Y] = ode15s(@(t,y) fun1(t,y),[t1 t0],[y1(0) y2(0) y3(0) y4(0)]) numerically
+%integrates from time t0 to time t1 with inital condition vector
+%[y1(0) y2(0) y3(0) y4(0)].
+
+[T,Y] = ode15s(@(t,y) fun2cell(t,y),[0 60],rand(1,4));
+
+%Here we plot morphogen concentrations as a function of time t.
+
+figure
+a1 = plot(T,Y(:,1),'-','LineWidth',3)
+hold on
+a2 = plot(T,Y(:,2),'-','LineWidth',3)
+hold on
+plot(T,Y(:,3),'-','LineWidth',3)
+hold on
+plot(T,Y(:,4),'-','LineWidth',3)
+set(gca,'FontSize',16,'LineWidth',2);
+title('Turing patterns')
+xlabel('time(s)');
+ylabel('Morphogen Concentration ');
+legend('morphogen X, cell 1', 'morphogen Y, cell 1','morphogen X, cell 2', 'morphogen Y, cell 2','Location','southwest');
---
+++ Turing_MATLAB/fun2cell.m
@@ -0,0 +1,17 @@
+% Here we define the system of DEs that 
+% describes Turing pattern formation in (Turing, 1952)
+% for the 2 cell, discrete system.
+function dy = fun2cell(t,y)
+a = -0.5;
+b = 1;
+c = -1;
+d = a + 1;
+mu = 0.42;
+nu = 0;
+%here we first initialize and then define the system of differential
+%equations
+dy = zeros(4,1); 
+dy(1) = a*y(1) + b*y(2)  + 2*mu*(y(3) - y(1));
+dy(2) = c*y(1) + d*y(2)  + 2*nu*(y(4) - y(2));
+dy(3) = a*y(3) + b*y(4)  + 2*mu*(y(1) - y(3));
+dy(4) = c*y(3) + d*y(4)  + 2*nu*(y(2) - y(4));
